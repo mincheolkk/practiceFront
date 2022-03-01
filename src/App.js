60baf41ff4da897e9b5588d127c7303107d1953e
mincheolkk <kmc04080408@gmail.com>
2022-03-01 22:57:20 +0900

    feat : Todo 리스트 등록

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import Todo from './Todo';
 import './App.css';
+import { Paper, List, Container } from '@material-ui/core';
+import AddTodo from './AddTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        items: [
+            {id:0, title:"hello world1", done:true},
+            {id:1, title:"hello world23", done:false},
+        ],
+    };
+  }
+
+  add = (item) => {
+    const thisItem = this.state.items;
+    item.id = "ID-" + thisItem.length; 
+    item.done = false;
+    thisItem.push(item);
+    this.setState({items:thisItem});
+    console.log("items : ", this.state.items);
+  }
+
+  render() {
+        var todoItems = this.state.items.length > 0 && (
+          <Paper style={{ margin: 16}}>
+            <List>
+              {this.state.items.map((item, idx) => (
+                <Todo item={item} key={item.id} />
+              ))}
+            </List>
+          </Paper>
+        );
+      
+        return (
+          <div className="App">
+            <Container maxWidth="md">
+              <AddTodo add={this.add} />
+              <div className="TodoList">{todoItems}</div>
+            </Container>
+           </div>
+          );
+  }
 }
-
 export default App;
